04575733ca5f44f4e38fd09bbbbed69cc4c1d3f5
Vasua Pupkin <timur.abdurahimov@yandex.ru>
2019-07-15 18:57:35 +0500

    Update 15.07.19 18:57

--- test/src/App.js
+++ test/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/img/Logo.png';
 import './App.css';
 
+console.log(logo);
+var version="1.0.0";
+
+function img(){
+    return(
+        <img src="{logo}" alt="Vitim+"></img>
+    )
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App" >
+            <header className="App-header">
+              <div id="top" name="top">
+                <div id="logo" >
+                {img()}
+                </div>
+              </div>
+            </header>
+            <div id="login">
+                <input id="in_email" type="text" required pattern="Username or Email"></input>
+                <input id="in_password" type="password" required pattern="Password"></input>
+                    <a href="">Forgot your password?</a>
+                <button type="submit" id="button">Login</button>
+                No have account? Sign up for free.
+                <a href=""><font id="sign_up">Sign up</font></a>
+            </div>
+            <div id="bottom">
+                <a href="">Support</a>
+                <div>Version {version} </div>
+                <a href="">Disclaimer</a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
